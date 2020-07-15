480c109771113716c1cf224238606c933b8d02a4
Noah <54943415+Spacetastics@users.noreply.github.com>
2020-07-15 12:51:58 -0400

    Problem 71A Solution
    Solution to #71A 'Way Too Long Words' in Java

---
+++ Code Forces/Difficulty 800/Problem71A.java
@@ -0,0 +1,34 @@
+//Solution to problem #71A: Way Too Long Words on the Code Forces problem set
+
+package difficulty800;
+
+import java.util.Scanner;
+
+public class Problem71A {
+
+	static Scanner input = new Scanner(System.in);
+
+	static String abbreviate(String word) {
+		int numLetters = word.length() - 2;
+		String temp = word;
+		word = word.substring(0, 1);
+		word += numLetters;
+		word += temp.substring(temp.length()-1);
+		return word;
+	}
+
+	public static void main(String[] args) {
+		int n = input.nextInt();
+		String[] words = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			words[i] = input.next();
+			if (words[i].length() > 10)
+				words[i] = abbreviate(words[i]);
+		}
+
+		for (String word : words) {
+			System.out.println(word);
+		}
+	}
+}
