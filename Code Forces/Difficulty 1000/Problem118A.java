e814968d5a555609d9ebeef417efebb40531becb
Noah <54943415+Spacetastics@users.noreply.github.com>
2020-07-21 21:28:03 -0400

    Problem 118A Solution
    Added comments

--- Code Forces/Difficulty 1000/Problem118A.java
+++ Code Forces/Difficulty 1000/Problem118A.java
@@ -8,14 +8,16 @@ import java.util.ArrayList;
 public class Problem118A {
 
 	static Scanner input = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
 		String string = input.nextLine();
+		//put input string into an ArrayList as individual characters in order to do various checks and transformations
 		ArrayList<String> array = new ArrayList<String>();
 		for (int i = 0; i < string.length(); i++) {
 			array.add(string.substring(i, i+1));
 		}
-		
+
+		//checks if current character in the ArrayList is a vowel or consonant in order to do the correct transformation
 		for (int i = 0; i < array.size(); i++) {
 			if (array.get(i).matches("a|e|i|o|u|y|A|E|I|O|U|Y")) {
 				array.remove(i);
@@ -29,10 +31,11 @@ public class Problem118A {
 				i++;
 			}
 		}
-		
+
+		//prints out the entire edited ArrayList out in the same line as opposed to printintg a string for the answer
 		for (int i = 0; i < array.size(); i++) {
 			System.out.print(array.get(i));
 		}
 	}
-	
+
 }
